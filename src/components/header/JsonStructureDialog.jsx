2d23bb8afcd911b71ff3f7ac4c35b845ec3fc27b
juan <jjhuertasbotache@gmail.com>
2024-12-01 20:17:42 -0500

    solved

--- src/components/header/JsonStructureDialog.jsx
+++ src/components/header/JsonStructureDialog.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
-import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from '../ui/dialog';
+import JSONPretty from 'react-json-pretty';
+import 'react-json-pretty/themes/monikai.css';
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogDescription } from '@/components/ui/dialog';
 import { Button } from '@/components/ui/button';
 import { ClipboardCheck, Clipboard } from 'lucide-react';
+
 
 export default function JsonStructureDialog({ jsonStructureOpen, setJsonStructureOpen, jsonStructure, copyJsonStructure, copied }) {
   return (
@@ -14,13 +16,12 @@ export default function JsonStructureDialog({ jsonStructureOpen, setJsonStructur
             Use it to ask to your preferred AI to generate a project for you.
           </DialogDescription>
         </DialogHeader>
-        <div className="dialog-content mt-2">
-          <pre className="p-2 rounded bg-gray-100 dark:bg-gray-700 relative">
-            {jsonStructure}
-            <Button onClick={copyJsonStructure} className="p-2 bg-blue-500 text-white rounded hover:bg-blue-600 absolute top-2 right-2">
-              {copied ? <ClipboardCheck size={16} /> : <Clipboard size={16} />}
-            </Button>
-          </pre>
+        <div className="dialog-content mt-2 relative max-w-full  overflow-x-auto">
+          <JSONPretty data={jsonStructure} theme={JSONPretty.monikai} className='text-xs p-1 border border-white rounded-lg' >
+          </JSONPretty>
+          <Button onClick={copyJsonStructure} className="absolute top-2 right-2">
+            {copied ? <ClipboardCheck size={16} /> : <Clipboard size={16} />}
+          </Button>
         </div>
       </DialogContent>
     </Dialog>
